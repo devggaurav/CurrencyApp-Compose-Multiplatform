1f327d4575b327532d4a87b9453db26f39962b0c
Gaurav <gaurav.chauhan@justanswer.com>
2024-04-28 19:50:23 +0530

    added dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.kotlinxSerialization)
+    alias(libs.plugins.realm.plugin)
 }
 
 kotlin {
@@ -31,14 +33,38 @@ kotlin {
         androidMain.dependencies {
             implementation(libs.compose.ui.tooling.preview)
             implementation(libs.androidx.activity.compose)
+            implementation(libs.ktor.client.android)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
-            implementation(compose.material)
+            implementation(compose.material3)
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
+
+            implementation(libs.koin.core)
+            implementation(libs.voyager.navigator)
+            implementation(libs.voyager.screenModel)
+            implementation(libs.voyager.transitions)
+            implementation(libs.voyager.koin)
+
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.ktor.serialization.kotlinx.json)
+
+            implementation(libs.kotlinx.datetime)
+
+            implementation(libs.multiplatform.settings.no.arg)
+            implementation(libs.multiplatform.settings.coroutines)
+
+            implementation(libs.mongodb.realm)
+            implementation(libs.kotlin.coroutines)
+            implementation(libs.stately.common)
+
+        }
+        iosMain.dependencies {
+         implementation(libs.ktor.client.darwin)
         }
     }
 }
---
+++ composeApp/src/commonMain/kotlin/data/remote/api/CurrencyApiService.kt
@@ -0,0 +1,9 @@
+package data.remote.api
+
+class CurrencyApiService {
+
+    companion object{
+        const val ENDPOINT = "https://api.currencyapi.com/v3/latest"
+        const val API_KEY = "cur_live_LuYRh5D9nzi7MMTDrYSISMWqPiJEIbvwWa2hlCU9"
+    }
+}
